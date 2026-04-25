d9cc87fb9d6091680fb1f7acfe582debd2e734df
Nguyen Van Huy <huynguyen2542004@gmail.com>
2026-04-25 10:15:45 +0700

    Update: Fix price display and deploy latest backend features

--- src/server.js
+++ src/server.js
@@ -11,10 +11,16 @@ const uploadRoutes = require('./routes/uploadRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
+
+// Request Logger
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // Connect Database
 connectDB();
